3b4d3a6357de5d2eae98d1c58ac4b3ae27382832
HETONGAPP <44877369+HETONGAPP@users.noreply.github.com>
2021-01-10 17:36:11 -0500

    Add files via upload

---
+++ mavsdk.sh
@@ -0,0 +1,15 @@
+##-------- Install MavSDK server for TX2 OR RASPI -------##
+##-------- Author: Tong He -------##
+##-------- Date: 10/01/2021 -------##
+
+
+sudo apt-get update -y
+sudo apt-get install cmake build-essential colordiff git doxygen -y
+git clone https://github.com/mavlink/MAVSDK.git
+cd MAVSDK
+git checkout master
+git submodule update --init --recursive
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_BACKEND=ON -Bbuild/default -H.
+cmake --build build/default
+
+
