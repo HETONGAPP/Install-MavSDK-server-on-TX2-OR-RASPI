e2ff1eec6759e4640c3c5fb01f50a2de9e95205e
HETONGAPP <44877369+HETONGAPP@users.noreply.github.com>
2021-01-12 21:58:11 -0500

    Update mavsdk.sh

--- mavsdk.sh
+++ mavsdk.sh
@@ -3,10 +3,6 @@
 ##-------- Date: 10/01/2021 -------##
 
 mv GCS.py /home/$USER/
-sudo apt update
-sudo apt upgrade
-sudo apt-get update -y
-sudo apt-get install cmake build-essential colordiff git doxygen -y
 git clone https://github.com/HETONGAPP/MAVSDK.git
 cd /$(pwd)/MAVSDK/
 source mavsdk_server.sh
